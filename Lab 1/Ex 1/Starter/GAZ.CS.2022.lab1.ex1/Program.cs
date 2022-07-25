d73c4ad0ffbe716c0a72c3fda245446d3943346d
Konstantinov <akonstantinov.2003.sokol@gmail.com>
2022-07-25 08:23:31 +0300

    gaz

---
+++ Lab 1/Ex 1/Starter/GAZ.CS.2022.lab1.ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAZ.CS._2022.lab1.ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                Console.WriteLine(line);
+            }
+
+           
+        }
+    }
+}
---
+++ Lab 2/Ex 1/Starter/GAZ.CS.2022.lab2.ex1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GAZ.CS._2022.lab2.ex1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            double numberDouble;
+            if (!double.TryParse(inputTextBox.Text, out numberDouble))
+            {
+                MessageBox.Show("Please enter a double");
+                return;
+            }
+
+            if (numberDouble <= 0)
+            {
+                MessageBox.Show("Please enter a positive number");
+                return;
+            }
+
+            double squareRoot = Math.Sqrt(numberDouble);
+
+            frameworkLabel.Content = string.Format("{0} (Using the .NET Framework)",
+           squareRoot);
+
+            decimal numberDecimal;
+            if (!decimal.TryParse(inputTextBox.Text, out numberDecimal))
+            {
+                MessageBox.Show("Please enter a decimal");
+                return;
+            }
+            decimal delta = Convert.ToDecimal(Math.Pow(10, -28));
+
+            decimal guess = numberDecimal / 2;
+            decimal result = ((numberDecimal / guess) + guess / 2);
+
+            while (Math.Abs(result - guess) > delta)
+            {
+                // Use the result from the previous iteration 
+                // as the starting point
+                guess = result;
+                // Try again
+                result = ((numberDecimal / guess) + guess) / 2;
+            }
+
+            newtonLabel.Content = string.Format("{0} (Using Newton)", result);
+        }
+    }
+
+    }
+
